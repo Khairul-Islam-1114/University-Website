3bbfc879c0ceaf398bf954d2206ea755cdbb9050
Khairul-Islam-1114 <khairulmuhammad1114@gmail.com>
2024-04-28 15:58:45 +0600

    Programs Section have been added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subtitle_1='O u r' subtitle_2='P r o g r a m s' main_title='What We Offers'/>
+        <Programs />
+
+
+      </div>
     </div>
   )
 }
