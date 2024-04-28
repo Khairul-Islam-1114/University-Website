6a1aa69f202a3821de6dffe525057e22f3a16a1e
Khairul-Islam-1114 <khairulmuhammad1114@gmail.com>
2024-04-28 18:01:16 +0600

    Updated Title component

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -5,7 +5,7 @@ const Title = ({subtitle_1,subtitle_2,main_title}) => {
   return (
     <div className='title'>
         <p><span>{subtitle_1}</span>  <span>{subtitle_2}</span></p>
-        <h2>{main_title}</h2>
+        <h2>{main_title }</h2>
     </div>
   )
 }
