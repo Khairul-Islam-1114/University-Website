a9d7068684dac02bce9ecaae4ea20b058614a82d
Khairul-Islam-1114 <khairulmuhammad1114@gmail.com>
2024-04-30 12:34:15 +0600

    The Navigation menu is now fully responsive for mobile view also by adding a menu-icon for the small screen that will toggle and be seen from sidebar.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/img/logo.png';
+import menu_icon from '../../assets/img/menu-icon.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -11,13 +12,19 @@ const Navbar = () => {
         window.addEventListener('scroll', ()=>{
             window.scrollY > 50 ? setSticky(true) : setSticky(false)
         })
-    },[])
+    },[]);
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    const toogleMenu = () =>{
+         !mobileMenu ? setMobileMenu(true) : setMobileMenu(false);
+    }
 
     return (
 
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo' />
-            <ul>
+            <ul className={mobileMenu ? 'hide-mobile-menu' : ''}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='programs' smooth={true} offset={-250} duration={500}>Programs</Link></li>
                 <li><Link to='about' smooth={true} offset={-50} duration={500}>About Us</Link></li>
@@ -27,6 +34,8 @@ const Navbar = () => {
                     <Link to='contact' smooth={true} offset={-205} duration={500} className='btn'>Contact Us</Link>
                 </li>
             </ul>
+
+            <img src={menu_icon} alt=""  className='menu-icon' onClick={toogleMenu}/>
         </nav>
     )
 }
