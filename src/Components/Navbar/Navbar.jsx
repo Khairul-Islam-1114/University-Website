56bb29251309890db2a7ad1641e2ed16ed7316d6
Khairul-Islam-1114 <khairulmuhammad1114@gmail.com>
2024-04-30 11:49:08 +0600

    React-Smooth-Scroll feature have been added to scrooll any particular section by clicking Nav-menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/img/logo.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -17,13 +18,13 @@ const Navbar = () => {
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo' />
             <ul>
-                <li>Home</li>
-                <li>Programms</li>
-                <li>About Us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='programs' smooth={true} offset={-250} duration={500}>Programs</Link></li>
+                <li><Link to='about' smooth={true} offset={-50} duration={500}>About Us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-250} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-140} duration={500}>Testimonials</Link></li>
                 <li>
-                    <button type="button" className='btn'>Contact Us</button>
+                    <Link to='contact' smooth={true} offset={-205} duration={500} className='btn'>Contact Us</Link>
                 </li>
             </ul>
         </nav>
